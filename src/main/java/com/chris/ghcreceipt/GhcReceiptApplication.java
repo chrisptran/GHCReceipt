8540eeeeed2acf75ad6d05393cebb28db1289592
chrisptran <christopher.p.tran@gmail.com>
2023-05-01 22:14:02 -0700

    new changes

---
+++ src/main/java/com/chris/ghcreceipt/GhcReceiptApplication.java
@@ -0,0 +1,13 @@
+package com.chris.ghcreceipt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhcReceiptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhcReceiptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chris/ghcreceipt/GhcReceiptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chris.ghcreceipt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhcReceiptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
